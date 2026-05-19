67578677260ae39528efddedf6cf22cc69f4b403
mexxxxx <fivan285@gmail.com>
2026-05-19 19:54:35 +0300

    change linux

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,7 @@
 package main
-
 import (
 	"mexxx1/golang-fullstack/config"
-	"mexxx1/golang-fullstack/internal/home"
-	"mexxx1/golang-fullstack/internal/vacancy"
+	"mexxx1/golang-fullstack/internal/home" "mexxx1/golang-fullstack/internal/vacancy"
 	"mexxx1/golang-fullstack/pkg/logger"
 	"mexxx1/golang-fullstack/pkg/logger/database"
 
@@ -26,6 +24,7 @@ func main() {
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: logger,
 	}))
+	
 	app.Use(recover.New())
 	app.Static("/public", "./public")
 
@@ -33,6 +32,7 @@ func main() {
 
 	dbpool := database.CreateDbPool(dbConf, logger)
 	defer dbpool.Close()
+
 
 	//Repo`s
 
